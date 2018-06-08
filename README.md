e8213fcc02fbe3faafbfbc02ffe8e7e54c1fad34
rocheio <andrewvroche@gmail.com>
2018-06-08 12:18:25 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Quake Timer
+
+Quake cooldown timer app for the game Quake Champions.
+
+Run the app:
+
+```sh
+go run main.go
+```
+
+Turn your volume up so you can hear the dings.
+
+Press the registered hotkey, even when terminal is not focused.
+
+`Alt + 1`
+
+You should hear a 'ding'
+
+## Credits
+
+- Bootstrapping of basic global hotkey listener: https://stackoverflow.com/a/38954281
---
+++ main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"syscall"
+	"time"
+	"unsafe"
+)
+
+const (
+	ModAlt = 1 << iota
+	ModCtrl
+	ModShift
+	ModWin
+)
+
+type HotkeyMessage struct {
+	HWND   uintptr
+	UINT   uintptr
+	WPARAM int16
+	LPARAM int64
+	DWORD  int32
+	POINT  struct{ X, Y int64 }
+}
+
+type Hotkey struct {
+	Id        int // Unique id
+	Modifiers int // Mask of modifiers
+	KeyCode   int // Key code, e.g. 'A'
+}
+
+// String returns a human-friendly display name of the hotkey
+// such as "Hotkey[Id: 1, Alt+Ctrl+O]"
+func (h *Hotkey) String() string {
+	mod := &bytes.Buffer{}
+	if h.Modifiers&ModAlt != 0 {
+		mod.WriteString("Alt+")
+	}
+	if h.Modifiers&ModCtrl != 0 {
+		mod.WriteString("Ctrl+")
+	}
+	if h.Modifiers&ModShift != 0 {
+		mod.WriteString("Shift+")
+	}
+	if h.Modifiers&ModWin != 0 {
+		mod.WriteString("Win+")
+	}
+	return fmt.Sprintf("Hotkey[Id: %d, %s%c]", h.Id, mod, h.KeyCode)
+}
+
+func main() {
+	user32 := syscall.MustLoadDLL("user32")
+	defer user32.Release()
+
+	regkey := user32.MustFindProc("RegisterHotKey")
+
+	hotkeys := map[int16]*Hotkey{
+		1: &Hotkey{1, ModAlt + ModCtrl, 'O'},  // ALT+CTRL+O
+		2: &Hotkey{2, ModAlt + ModShift, 'M'}, // ALT+SHIFT+M
+		3: &Hotkey{3, ModAlt + ModCtrl, 'X'},  // ALT+CTRL+X
+		4: &Hotkey{4, ModAlt, '1'},  // F1
+	}
+
+	for _, h := range hotkeys {
+    	r1, _, err := regkey.Call(
+    		0, uintptr(h.Id), uintptr(h.Modifiers), uintptr(h.KeyCode))
+    	if r1 == 1 {
+    		fmt.Println("Registered", h)
+    	} else {
+    		fmt.Println("Failed to register", h, ", error:", err)
+    	}
+	}
+
+	peekmsg := user32.MustFindProc("PeekMessageW")
+
+	for {
+		var msg = &HotkeyMessage{}
+		peekmsg.Call(uintptr(unsafe.Pointer(msg)), 0, 0, 0, 1)
+
+		// Registered id is in the WPARAM field:
+		if id := msg.WPARAM; id != 0 {
+			fmt.Println("Hotkey pressed:", hotkeys[id])
+			if id == 3 { // CTRL+ALT+X = Exit
+				fmt.Println("CTRL+ALT+X pressed, goodbye...")
+				return
+			}
+            if id == 4 { // ALT+1 = Ding
+                fmt.Println("Play audio here...")
+            }
+		}
+
+		time.Sleep(time.Millisecond * 50)
+	}
+}
